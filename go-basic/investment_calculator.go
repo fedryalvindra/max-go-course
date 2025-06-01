92ffc3e7ad95e6584dc3b78c84329617308d50f1
Fedry Alvindra <114293869+fedryalvindra@users.noreply.github.com>
2025-06-01 16:02:20 +0700

    init

---
+++ go-basic/investment_calculator.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	const inflationRate float64 = 6.5
+	var investmentAmount float64 = 1000
+	expectedReturnRate := 5.5
+	var years float64 = 10
+
+	investmentAmount = 2000
+
+	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+
+	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
+}
