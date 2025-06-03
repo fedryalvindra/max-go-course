53282b1572e005219bf43357b576737b3bee7558
Fedry Alvindra <114293869+fedryalvindra@users.noreply.github.com>
2025-06-03 22:32:57 +0700

    function return multiple values

--- go-basic/investment_calculator.go
+++ go-basic/investment_calculator.go
@@ -5,10 +5,11 @@ import (
 	"math"
 )
 
+const inflationRate float64 = 6.5
+
 func main() {
-	const inflationRate float64 = 6.5
 	var investmentAmount float64
-	var expectedReturnRate float64
+	var expectedReturnRate float64 = 5.5
 	var years float64
 
 	outputText("Investment Amount: ")
@@ -18,8 +19,9 @@ func main() {
 	outputText("Years: ")
 	fmt.Scan(&years)
 
-	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
-	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+	futureValue, futureRealValue := calculateFutureValues(investmentAmount, expectedReturnRate, years)
+	// futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	// futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 
 	formattedFV := fmt.Sprintf("Future Value: %.1f\n", futureValue)
 	formattedRFV := fmt.Sprintf("Future Value (adjusted for Inflation):%.1f\n", futureRealValue)
@@ -32,4 +34,10 @@ func main() {
 
 func outputText(text string) {
 	fmt.Print(text)
+}
+
+func calculateFutureValues(investmentAmount, expectedReturnRate, years float64) (float64, float64) {
+	fv := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	rfv := fv / math.Pow(1+inflationRate/100, years)
+	return fv, rfv
 }
