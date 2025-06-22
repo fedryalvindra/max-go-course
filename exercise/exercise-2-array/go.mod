1785b8217a60e1ea9d4e61dc51f8e41423ff4021
Fedry Alvindra <114293869+fedryalvindra@users.noreply.github.com>
2025-06-22 18:54:29 +0700

    map and for loop

---
+++ exercise/exercise-2-array/go.mod
@@ -0,0 +1,3 @@
+module exercise-2-array
+
+go 1.24.1
--- go-array/lists/list.go
+++ go-array/lists/list.go
@@ -1,4 +1,4 @@
-package main
+package lists
 
 import (
 	"fmt"
@@ -14,9 +14,13 @@ func main() {
 	prices := []float64{10.99, 8.99}
 	fmt.Println(prices[0:1])
 	prices[1] = 9.99
-	
-	prices = append(prices, 5.99)
+
+	prices = append(prices, 5.99, 12.99)
 	prices = prices[1:]
+	fmt.Println(prices)
+
+	discountPrices := []float64{101.99, 80.99, 20.59}
+	prices = append(prices, discountPrices...)
 	fmt.Println(prices)
 }
 
